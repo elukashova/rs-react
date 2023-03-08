2815cc3c358797ffaf4c923e91a8375cba78ddb1
elukashova <ele.lukashova@gmail.com>
2023-03-08 13:38:33 +0100

    feat: implement routing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './index.css';
+import { Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from './pages/404';
+import { MainPage } from './pages/Main';
+import { AboutPage } from './pages/About';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ react-app/src/pages/404.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotFoundPage = () => {
+  return <header>404 Not Found page</header>;
+};
