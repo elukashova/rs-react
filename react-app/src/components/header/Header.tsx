439da274ceb437ee5ebafb28b908501ae8f3df12
elukashova <ele.lukashova@gmail.com>
2023-03-08 22:00:14 +0100

    feat: add header

---
+++ react-app/src/components/header/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.styles.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <h1>React App</h1>
+        <nav className="menu">
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+            to="/"
+            end
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending ? 'pending' : isActive ? 'active' : ''
+            }
+            to="/about"
+          >
+            About us
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
