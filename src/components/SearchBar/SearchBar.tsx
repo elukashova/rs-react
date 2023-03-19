5487705ce9ed83253f8c62297ccf2fdcbb295136
elukashova <ele.lukashova@gmail.com>
2023-03-19 10:34:11 +0100

    refactor: change project structure

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, ChangeEventHandler, Component } from 'react';
+import ButtonSearch from './Button/ButtonSearch';
 import styles from './SearchBar.module.css';
 
 class SearchBar extends Component<Record<string, never>, { value: string }> {
@@ -35,13 +36,7 @@ class SearchBar extends Component<Record<string, never>, { value: string }> {
           placeholder="Search..."
           autoComplete="off"
         />
-        <button className={styles.btn}>
-          <svg className={styles.icon} aria-hidden="true" viewBox="0 0 24 24">
-            <g>
-              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-            </g>
-          </svg>
-        </button>
+        <ButtonSearch />
       </div>
     );
   }
