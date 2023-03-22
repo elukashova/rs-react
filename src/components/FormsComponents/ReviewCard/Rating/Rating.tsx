2fda5bf0ec9c173172b92fd4b7cf585aba12751e
elukashova <ele.lukashova@gmail.com>
2023-03-22 17:17:20 +0100

    feat: add cards

--- src/components/FormsComponents/ReviewCard/Rating/Rating.tsx
+++ src/components/FormsComponents/ReviewCard/Rating/Rating.tsx
@@ -16,11 +16,14 @@ class Rating extends React.Component<{ rating: string }> {
   render(): JSX.Element {
     return (
       <div className={styles.rating}>
-        <span className={styles[this.defineClass(1)]}></span>
-        <span className={styles[this.defineClass(2)]}></span>
-        <span className={styles[this.defineClass(3)]}></span>
-        <span className={styles[this.defineClass(4)]}></span>
-        <span className={styles[this.defineClass(5)]}></span>
+        Review:
+        <div className={styles.stars}>
+          <span className={styles[this.defineClass(1)]}></span>
+          <span className={styles[this.defineClass(2)]}></span>
+          <span className={styles[this.defineClass(3)]}></span>
+          <span className={styles[this.defineClass(4)]}></span>
+          <span className={styles[this.defineClass(5)]}></span>
+        </div>
       </div>
     );
   }
--- src/components/FormsComponents/ReviewCard/ReviewCard.tsx
+++ src/components/FormsComponents/ReviewCard/ReviewCard.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 import Review from './ReviewCard.types';
 import styles from './ReviewCard.module.css';
 import Rating from './Rating/Rating';
+import PrivacyConsent from './Privacy/Privacy';
 
 class ReviewCard extends React.Component<Review> {
+  formatDate(date: string): string {
+    return `${new Date(date).toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    })}`;
+  }
+
   render(): JSX.Element {
     const { image, name, hut, arrival, departure, rating } = this.props;
     return (
       <div className={styles.card}>
         <div className={styles.user}>
           <img className={styles.image} alt="user avatar" src={image} />
-          <p className={styles.name}>{name}</p>
+          <div className={styles.info}>
+            <p className={styles.name}>{name}</p>
+            <PrivacyConsent />
+          </div>
         </div>
         <div className={styles.review}>
-          <p className={styles.hut}>{hut}</p>
-          <p className={styles.stay}>
-            {arrival} - {departure}
-          </p>
+          <div className={styles.hut}>
+            <p className={styles.heading}>Hut:</p> {hut}
+          </div>
+          <div className={styles.stay}>
+            <p className={styles.heading}>Travel period:</p> {this.formatDate(arrival)} -{' '}
+            {this.formatDate(departure)}
+          </div>
           <Rating rating={rating} />
         </div>
       </div>
