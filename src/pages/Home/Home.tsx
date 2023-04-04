9dbea84bad4305a468a0abbb671fb237d08440bf
elukashova <ele.lukashova@gmail.com>
2023-04-05 00:09:37 +0200

    feat: add loader

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import Hut from '../../components/HomeComponents/Card/Card.types';
 import Catalogue from '../../components/HomeComponents/Catalogue/Catalogue';
+import ProgressIndicator from '../../components/HomeComponents/Loader/Loader';
 import SearchBar from '../../components/HomeComponents/SearchBar/SearchBar';
 import { API_PATH } from '../../utils/consts';
 import styles from '../Layout.module.css';
 
 const HomePage = (): JSX.Element => {
+  const [firstLoad, setFirstLoad] = useState<boolean>(true);
   const [huts, setHuts] = useState<Hut[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const filterByQuery = (filteredHuts: Hut[]) => {
-    setHuts(filteredHuts);
+    setLoading(true);
+    setTimeout(() => {
+      setHuts(filteredHuts);
+      setLoading(false);
+    }, 500);
   };
 
   useEffect(() => {
-    fetch(API_PATH)
-      .then((response: Response) => response.json())
-      .then((hutsData: Hut[]) => {
-        setHuts(hutsData);
-      });
-  }, []);
+    if (firstLoad) {
+      setLoading(true);
+      setTimeout(() => {
+        fetch(API_PATH)
+          .then((response: Response) => response.json())
+          .then((hutsData: Hut[]) => {
+            setHuts(hutsData);
+            setLoading(false);
+          });
+        setFirstLoad(false);
+      }, 1000);
+    }
+  }, [firstLoad]);
 
   return (
     <section className={styles.section}>
       <SearchBar filterHuts={filterByQuery} />
-      <Catalogue currentHuts={huts} />
+      {loading && <ProgressIndicator />}
+      {!loading && <Catalogue currentHuts={huts} />}
     </section>
   );
 };
