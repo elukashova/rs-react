b4d661194acf921f32b2beb8304331e2b8f0a0e5
elukashova <ele.lukashova@gmail.com>
2023-04-26 10:16:36 +0200

    feat: move app to server in dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ROUTES from './utils/router/router';
 import './App.css';
 
 const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.path} element={ROUTES.element}>
-          {ROUTES.subroutes.map(({ key, index, path, element }) => {
-            return <Route key={key} path={index ? '' : path} index={index} element={element} />;
-          })}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path={ROUTES.path} element={ROUTES.element}>
+        {ROUTES.subroutes.map(({ key, index, path, element }) => {
+          return <Route key={key} path={index ? '' : path} index={index} element={element} />;
+        })}
+      </Route>
+    </Routes>
   );
 };
 
